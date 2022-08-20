697a9a0ddcfb0ecb197323c92e4cef865e2242ee
Sulayman Matkuliyev <suleymantm11@gmail.com>
2022-08-20 15:55:15 +0300

    setOperators

---
+++ day4_set1.sql
@@ -0,0 +1,32 @@
+SELECT * FROM departments ORDER BY department_id;
+
+CREATE TABLE DEPT1
+AS SELECT * FROM departments WHERE department_id <= 160;
+
+CREATE TABLE DEPT2
+AS SELECT * FROM departments WHERE department_id >= 100;
+
+SELECT * FROM dept1 ORDER BY 1;
+
+SELECT * FROM dept1
+UNION
+SELECT * FROM dept2
+ORDER BY 1;
+
+-- KOLON SAYILARI AYNI OLMASI LAZIM, UNION VE UNION ALL ÝÇÝN
+-- KOLON SAYISI VE VERÝ TÝPÝ AYNI OLMASI LAZIM
+SELECT department_id, department_name FROM dept1
+UNION
+SELECT department_id, department_name FROM dept2
+ORDER BY 1;
+
+-- INTERSECT & MINUS
+SELECT * FROM dept1
+INTERSECT
+SELECT * FROM dept2;
+
+SELECT * FROM dept2
+MINUS
+SELECT * FROM dept1;
+
+
